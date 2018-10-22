8bc48e4a1afc170114885f03173c419f678f6c59
Nelson Pecora <nelson@yoshokatana.com>
2018-10-21 20:10:11 -0400

    set response header

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Content-Type", "application/json");
 
   next();
 });
